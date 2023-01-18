5829c4a1d6b5bcb5efa712d0792b13700ecba378
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-01-18 05:39:15 +0530

    Added DLL Linked Lists

--- Data Structure/doubleLL.c
+++ Data Structure/doubleLL.c
@@ -1,84 +1,86 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-struct node {
+struct node{
+    int data;
     struct node* next;
     struct node* prev;
-    int data;
 };
 struct node* head = NULL;
 
-void insertAtStart(int data){
+void inserAtBegining(int data){
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->data = data;
     new_node->next = NULL;
     new_node->prev = NULL;
-    if(head != NULL){
-        new_node->next = head;
+    
+    if(head==NULL){
         head = new_node;
-    }else{
-        head = new_node;
+    }
+    else{
+        new_node->next=head;
+        head->prev=new_node;
+        head=new_node;
     }
 }
 
-void insertAtLast(int data){
-    struct node* ptr = head;
+void inserAtLast(int data){
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->data = data;
     new_node->next = NULL;
     new_node->prev = NULL;
-    while(ptr->next != NULL){
-        ptr= ptr->next;
+    
+    struct node* ptr=head;
+    
+    if(head==NULL){
+        head = new_node;
     }
-    new_node->prev = ptr;
-    ptr->next = new_node;
+    else{
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        }
+        new_node->prev=ptr;
+        ptr->next=new_node;
+    }
 }
 
-void insertAtPosition(int pos, int data){
-    struct node* ptr = head;
+void inserAtPos(int pos,int data){
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
     new_node->next = NULL;
     new_node->prev = NULL;
-    new_node->data = data;
-    int i = 1;
-    while(i<pos-1){
+    
+    struct node* ptr=head;
+        
+    for(int i=1; i<pos-1;i++){
         ptr=ptr->next;
-        i++;
     }
-
     new_node->prev=ptr;
     new_node->next=ptr->next;
-
-    // ptr->next->prev=new_node;
+    ptr->next->prev=new_node;
     ptr->next=new_node;
-    new_node->next->prev=new_node;
-
-    // new_node->next = ptr;
-    // new_node->prev = ptr->prev;
-    // ptr->prev = new_node;
-    // new_node->prev->next = new_node;
-    
-    // ptr->prev->next = new_node;
-    // new_node->prev = ptr->prev;
-    // ptr->prev = new_node;
-    // new_node->next = ptr;
 }
 
 void display(){
-    struct node* ptr1 = head;
-    while(ptr1!=NULL){
-        printf("%d ",ptr1->data);
-        ptr1 = ptr1->next;
+    struct node *ptr=head;
+    if(head==NULL){
+        printf("Stack is Empty");
+    }
+    else{
+        while(ptr!=NULL){
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+        }
     }
 }
+    
 
 int main(){
-    insertAtStart(1);
-    insertAtStart(2);
-    insertAtStart(3);
-    insertAtStart(4);
-    insertAtLast(5);
-    insertAtPosition(3,9);
+    inserAtBegining(10);
+    inserAtBegining(20);
+    inserAtBegining(40);
+    inserAtLast(0);
+    inserAtPos(3,30);
     display();
+    
 }
